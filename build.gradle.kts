f69eb518e3a96133c53d7e0878f2bf64bb5a0a45
Albert Casanovas <caay2000@gmail.com>
2022-12-02 00:29:05 +0100

    Adding project structure

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    application
+    kotlin("jvm") version "1.7.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+}
+
+group = "com.github.caay2000"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "7.5"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
